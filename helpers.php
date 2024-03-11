946333335fcc8795cf4ab155e0743cb6a9a2a56f
John Ferreira <jpffm2018@gmail.com>
2024-03-11 06:35:40 +0530

    partials code added

--- helpers.php
+++ helpers.php
@@ -8,5 +8,47 @@
  */
 
  function basePath($path){
-    return __DIR__.$path;
+    $baseName =  __DIR__.$path;
+
+    if(file_exists($baseName)){
+      require $baseName;
+    } else {
+      echo "View '{$path}' not found";
+    }
  }
+
+/**
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+
+ function loadView($name){
+   $viewPath = basePath("/views/partials/{$name}.php");
+
+   if(file_exists($viewPath)) {
+      require $viewPath;
+   } else {
+      echo "View '{$name}' not found";
+   }
+
+ } 
+
+ /**
+ * 
+ * @param string $name
+ * @return void
+ * 
+ */
+
+ function loadPartial($name){
+   $partialPath = basePath("/views/partials/{$name}.php");
+
+   if(file_exists($partialPath)) {
+      require $partialPath;
+   } else {
+      echo "View '{$name}' not found";
+   }
+
+ } 
